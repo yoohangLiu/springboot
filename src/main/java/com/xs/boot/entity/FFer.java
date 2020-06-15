03d7d6ea3feecfda78b49f9296c231839dfc84cf
FF <krisandjake@foxmail.com>
2020-06-15 22:46:46 +0800

    FF test for entity.

---
+++ src/main/java/com/xs/boot/entity/FFer.java
@@ -0,0 +1,33 @@
+package com.xs.boot.entity;
+
+/**
+ * Created by FFer on 2020/6/15.
+ */
+public class FFer {
+    private String name;
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "FFer{" +
+                "name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
